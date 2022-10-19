8ea8b45c87a6e6ab892a23557954a18d577463ee
Nordleen De Frias <nordleendefrias@gmail.com>
2022-10-19 17:20:50 -0400

    Populated db files, schema.sql, and seeds.sql, created tables view data

--- db/schema.sql
+++ db/schema.sql
@@ -4,30 +4,30 @@ CREATE DATABASE hr_db;
 USE hr_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
-    FOREIGN KEY (department_id),
+    department_id INT,
+    FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    FOREIGN KEY (role_id),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
     REFERENCES role(id)
-    ON DELETE SET NULL,
-    manager_id INT
+    ON DELETE SET NULL
 );
 
-ALTER TABLE employee ADD CONSTRAINT fk_employee
-    FOREIGN KEY (id) REFERENCES employee(id);
+-- ALTER TABLE employee ADD CONSTRAINT fk_employee
+--     FOREIGN KEY (id) REFERENCES employee(id);
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+const inquirer = require('inquirer');
 const express = require('express');
+const cTable = require('console.table');
 const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -12,11 +15,47 @@ const db = mysql.createConnection(
   {
     host: 'localhost',
     user: 'root',
-    password: '######',
+    password: 'Gold2022##',
     database: 'hr_db'
   },
-  console.log(`Connected to the classlist_db database.`)
 );
+
+//confirm connection
+db.connect(function(error) {
+  if(error) {
+    throw error;
+  } else {
+    console.log(`Connected to the Human Resources database.`)
+  }
+});
+
+// Welcome message
+console.log("<><><><> WELCOME TO THE EMPLOYEE MANAGER <><><><>");
+
+// Start Questionaire
+const questions = () => {
+  const answers = inquirer.prompt([
+    {
+      type: 'list',
+      name: 'toDonext',
+      message: 'What would you like to do?',
+      choices: ['view all departments','view all roles','view all employees','add a department','add a role','add an employee','update an employee role']
+    }
+    
+  ])
+  if(answers.toDoNext === 'view all departments') {
+    db.query('SELECT * FROM departments', function (err, results) {
+      console.log(results);
+    }); 
+  } else if (answers.toDoNext ==='view all roles') {
+    db.query('SELECT * FROM role', function (err, results) {
+      console.log(results);
+    });
+  } else if (answers.toDoNext ==='view all employees') {
+    db.query('SELECT * FROM employee', function (err, results) {
+      console.log(results);
+    });
+}
 
 
 // Default response for any other request (Not Found)
@@ -27,4 +66,5 @@ app.use((req, res) => {
 //port
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-  });
+  })
+};
