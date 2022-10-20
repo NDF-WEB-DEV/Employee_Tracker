d4cca62ea3c4cb227c47d1b9f5a868c668eec8bd
Nordleen De Frias <nordleendefrias@gmail.com>
2022-10-20 00:03:01 -0400

    updating server.js file, started to write functions, added .env file, started query.sql file

---
+++ db/query.sql
@@ -0,0 +1,11 @@
+-- View roles TABLE
+SELECT * FROM role;
+
+-- View Employee TABLE
+SELECT * FROM employee;
+
+-- View Departments TABLE
+SELECT * FROM department;
+
+
+
--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cTable = require('console.table');
 const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -19,6 +20,10 @@ const db = mysql.createConnection(
     database: 'hr_db'
   },
 );
+// not sure about this ask TA
+process.env.DATABASE, 
+process.env.USERNAME, 
+process.env.PASSWORD,
 
 //confirm connection
 db.connect(function(error) {
@@ -27,35 +32,92 @@ db.connect(function(error) {
   } else {
     console.log(`Connected to the Human Resources database.`)
   }
-});
 
-// Welcome message
 console.log("<><><><> WELCOME TO THE EMPLOYEE MANAGER <><><><>");
-
-// Start Questionaire
-const questions = () => {
-  const answers = inquirer.prompt([
-    {
-      type: 'list',
-      name: 'toDonext',
-      message: 'What would you like to do?',
-      choices: ['view all departments','view all roles','view all employees','add a department','add a role','add an employee','update an employee role']
+//INQUIRER QUESTIONS START HERE
+const answers = inquirer.prompt([
+  {
+    type: 'list',
+    name: 'toDoNext',
+    message: 'What would you like to do?',
+    choices: ['view all departments','view all roles','view all employees','add a department','add a role','add an employee','update an employee role']
+  } 
+  ]).then(answers); {
+    if(answers == 'view all departments') {
+      viewAllDepartments();
+    } else if (answers =='view all roles') {
+      viewAllRoles();
+    } else if (answers =='view all employees') {
+      viewAllEmployees();
+    } else if (answers == 'add a department') {
+      addDepartment();
+    } else if (answers == 'add a role') {
+      addRole();
+    } else if (answers == "add an employee") {
+      addEmployee();
+    } else if (answers == "update an employee role") {
+      updateEmployee();
     }
-    
-  ])
-  if(answers.toDoNext === 'view all departments') {
-    db.query('SELECT * FROM departments', function (err, results) {
-      console.log(results);
-    }); 
-  } else if (answers.toDoNext ==='view all roles') {
+  };
+
+  //VIEW ALL DEPARTMENTS
+function viewAllDepartments() {
+    db.query('SELECT * FROM department', function (err, results) {
+      if(err) throw err;
+      console.table(results);
+  });
+};
+
+//VIEW ALL ROLES
+function viewAllRoles(){
     db.query('SELECT * FROM role', function (err, results) {
-      console.log(results);
+      if(err) throw(err);
+      console.table(results);
+  });
+};
+
+//VIEW ALL EMPLOYEES
+function viewAllEmployees() {
+  db.query('SELECT * FROM employee', function (err, results) {
+    if(err) throw(err);
+    console.table(results);
+  });
+};
+
+// ADD DEPARTMENT
+function addDepartment() {
+  inquirer.prompt ([
+    {
+      type: 'input',
+      name: 'department',
+      message: 'Please enter the name of the department'
+    }
+  ]).then(function(answers){
+      db.query('INSERT INTO department VALUES (DEFAULT, ?'), [answers.department], function(err) {
+          if(err) throw(err);
+          console.log("Department" + answers.department + "was added to the list");
+      }
     });
-  } else if (answers.toDoNext ==='view all employees') {
-    db.query('SELECT * FROM employee', function (err, results) {
-      console.log(results);
-    });
+  };
+
+//ADD ROLE
+function addRole() {
+
+};
+
+//ADD EMPLOYEE
+function addEmployee() {
+
+};
+
+//UPDATE EMPLOYEE
+function updateEmployee() {
+  
 }
+
+
+
+
 
 
 // Default response for any other request (Not Found)
@@ -66,5 +128,5 @@ app.use((req, res) => {
 //port
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-  })
-};
+  });
+});
